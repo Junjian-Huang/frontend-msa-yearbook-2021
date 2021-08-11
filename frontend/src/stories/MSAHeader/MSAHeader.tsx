0fbcd2fd988aa854da5388730e622e125e20bf57
junjian.huang@hotmail.com <junjian.huang@hotmail.com>
2021-08-11 15:53:36 +1200

    add a submit form

--- frontend/src/stories/MSAHeader/MSAHeader.tsx
+++ frontend/src/stories/MSAHeader/MSAHeader.tsx
@@ -1,1 +1,40 @@
 import React from "react";
+import {AppBar, Button, createStyles, IconButton, makeStyles, Toolbar, Typography} from '@material-ui/core';
+import { Dehaze } from "@material-ui/icons";
+
+const useStyles = makeStyles(createStyles({
+    root: {
+        width: '100%'
+    },
+    menuButton: {
+
+    },
+    title: {
+
+    }
+}));
+
+export default function MSAHeader() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <Dehaze />
+            </IconButton>
+            <Typography className={classes.title} variant="h6">
+              My super cool web app
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
